cb64a1926cacf3ebbb3b4f2549fd27af8d36dfd3
francienaumento01 <francienaumento01@gmail.com>
2026-06-10 18:51:48 +0800

    User Greeting

---
+++ TRACKv2/web-user/src/components/common/UserGreeting.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const UserGreeting = () => {
+    
+  return (
+    <div>
+      <div>
+        <h1>Welcome, <span>Shiela Marie Garcia!</span>!</h1>
+      </div>
+      <div>
+        <p>Dean |<span>CICS Department</span> </p>
+      </div>
+    </div>
+  );
+};
+
+export default UserGreeting;
